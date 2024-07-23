d9dcc715f5530d036e32329678228e8a6d2f948e
Milena <100636013+milena579@users.noreply.github.com>
2024-07-23 14:33:17 -0300

    Create ParOuImpar.c

---
+++ Exercicios Diversos/ParOuImpar.c
@@ -0,0 +1,19 @@
+//Utilizando operadores matemáticos, desenvolva um código para detectar se um número inteiro é par ou ímpar. 
+
+#include<stdio.h> 
+
+int main(){ 
+
+    int numero = 11; 
+
+    if(numero %2 == 0){ 
+
+        printf("O numero %d eh par!", numero); 
+
+    } else{ 
+
+        printf("O numero %d eh impar!", numero); 
+
+    } 
+
+} 
